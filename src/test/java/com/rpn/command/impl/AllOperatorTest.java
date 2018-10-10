8f0f4a936cb0690772748bdfa7aca4662fbf0283
VamsiKDimmiti <dimmitivamsikrishna@gmail.com>
2018-10-10 23:05:20 +1100

    Added Unit Test Cases

--- src/test/java/com/rpn/command/impl/AllOperatorTest.java
+++ src/test/java/com/rpn/command/impl/AllOperatorTest.java
@@ -14,166 +14,84 @@ public class AllOperatorTest {
 
     private RPNParam rpn;
     private Operator operator;
-    private StrOperator strOperator;
 
 
     @Before
-    public void init(){
+    public void init() {
         rpn = new RPNParam();
         rpn.setFirstOperand(4.0);
         rpn.setSecondOperand(6.0);
     }
+
     @Test
     public void testAddCompute() {
         rpn.setOperator("+");
-        operator =  new AddOperator(rpn);
+        operator = new AddOperator(rpn);
         Assert.assertEquals(10.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testNegativeAddCompute() {
         rpn.setOperator("+");
-        operator =  new AddOperator(rpn);
+        operator = new AddOperator(rpn);
         Assert.assertNotEquals(8.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testSubCompute() {
         rpn.setOperator("-");
-        operator =  new SubOperator(rpn);
+        operator = new SubOperator(rpn);
         Assert.assertEquals(2.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testNegSubCompute() {
         rpn.setOperator("-");
-        operator =  new SubOperator(rpn);
+        operator = new SubOperator(rpn);
         Assert.assertNotEquals(4.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testMultipleCompute() {
         rpn.setOperator("*");
-        operator =  new MultiplicateOperator(rpn);
+        operator = new MultiplicateOperator(rpn);
         Assert.assertEquals(24.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testNegMultipleCompute() {
         rpn.setOperator("*");
-        operator =  new MultiplicateOperator(rpn);
+        operator = new MultiplicateOperator(rpn);
         Assert.assertNotEquals(2.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testDivisionCompute() {
         rpn.setOperator("/");
-        operator =  new DivisionOperator(rpn);
+        operator = new DivisionOperator(rpn);
         Assert.assertEquals(1.5, operator.compute(), 0.0);
     }
 
     @Test
     public void testNegDivisionCompute() {
         rpn.setOperator("/");
-        operator =  new DivisionOperator(rpn);
-        Assert.assertNotEquals(2.0, operator.compute(), 0.0);
+        operator = new DivisionOperator(rpn);
+        Assert.assertNotEquals(3.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testSqrtCompute() {
         rpn.setOperator("sqrt");
         rpn.setFirstOperand(9.0);
-        operator =  new SqrtOperator(rpn);
-        Assert.assertEquals(3, operator.compute(), 0.0);
+        operator = new SqrtOperator(rpn);
+        Assert.assertEquals(3.0, operator.compute(), 0.0);
     }
 
     @Test
     public void testNegSqrtCompute() {
         rpn.setOperator("sqrt");
         rpn.setFirstOperand(9.0);
-        operator =  new SqrtOperator(rpn);
+        operator = new SqrtOperator(rpn);
         Assert.assertNotEquals(2.0, operator.compute(), 0.0);
-    }
-
-    @Test
-    public void testStrAddOperatorResult(){
-        rpn.setOperator("+");
-        strOperator = new StrAddOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertEquals(10, op.compute(),0.0);
-    }
-
-    @Test
-    public void testNegStrAddOperatorResult(){
-        rpn.setOperator("+");
-        strOperator = new StrAddOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertNotEquals(6, op.compute(),0.0);
-    }
-
-    @Test
-    public void testStrSubOperatorResult(){
-        rpn.setOperator("-");
-        strOperator = new StrSubOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertEquals(2, op.compute(),0.0);
-    }
-
-    @Test
-    public void testNegStrSubOperatorResult(){
-        rpn.setOperator("-");
-        strOperator = new StrSubOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertNotEquals(7, op.compute(),0.0);
-    }
-
-    @Test
-    public void testStrDivOperatorResult(){
-        rpn.setOperator("/");
-        strOperator = new StrDivisionOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertEquals(1.5, op.compute(),0.0);
-    }
-
-    @Test
-    public void testNegStrDivOperatorResult(){
-        rpn.setOperator("/");
-        strOperator = new StrDivisionOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertNotEquals(6, op.compute(),0.0);
-    }
-
-    @Test
-    public void testStrMulOperatorResult(){
-        rpn.setOperator("*");
-        strOperator = new StrMultiplicateOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertEquals(24, op.compute(),0.0);
-    }
-
-    @Test
-    public void testNegMulOperatorResult(){
-        rpn.setOperator("*");
-        strOperator = new StrMultiplicateOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertNotEquals(20, op.compute(),0.0);
-    }
-
-    @Test
-    public void testStrSqrtOperatorResult(){
-        rpn.setOperator("sqrt");
-        rpn.setFirstOperand(9.0);
-        strOperator = new StrSqrtOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertEquals(3, op.compute(),0.0);
-    }
-
-    @Test
-    public void testNegSqrtOperatorResult(){
-        rpn.setOperator("sqrt");
-        rpn.setFirstOperand(9.0);
-        strOperator = new StrSqrtOperator();
-        Operator op = strOperator.getResult(rpn);
-        Assert.assertNotEquals(7, op.compute(),0.0);
     }
 }
